98de2c283d13b8658e27bddcaffde524f47bafbc
Sagar Shah <sagar@letsdothis.com>
2023-12-03 11:33:55 +0000

    WIP day 3 part 1, again test case passes but main set does not

--- day3/solve.ts
+++ day3/solve.ts
@@ -1,74 +1,131 @@
 import { parseLines, solve } from '../utils/typescript';
 
-// is a string a number
-const isNumber = (input: string | number) => {
-  return !isNaN(Number(input.toString()));
+type numberData = {
+  value: number;
+  x1: number;
+  y: number;
+  x2: number;
 };
 
-const isSymbol = (input: string) => input !== '.' && !isNumber(input);
-
-const checkIfSymbolNearby = (
-  input: string[],
-  i: number,
-  j: number,
-  width: number,
-  height: number,
-) => {
-  const left = j - 1 >= 0 ? input[i][j - 1] : null;
-  const right = j + 1 < width ? input[i][j + 1] : null;
-  const up = i - 1 >= 0 ? input[i - 1][j] : null;
-  const down = i + 1 < height ? input[i + 1][j] : null;
-  const upLeft = i - 1 >= 0 && j - 1 >= 0 ? input[i - 1][j - 1] : null;
-  const upRight = i - 1 >= 0 && j + 1 < width ? input[i - 1][j + 1] : null;
-  const downLeft = i + 1 < height && j - 1 >= 0 ? input[i + 1][j - 1] : null;
-  const downRight =
-    i + 1 < height && j + 1 < width ? input[i + 1][j + 1] : null;
-
-  const checks = [
-    left,
-    right,
-    up,
-    down,
-    upLeft,
-    upRight,
-    downLeft,
-    downRight,
-  ].filter((direction) => direction !== null);
-
-  return checks.some(isSymbol);
+type symbolData = {
+  symbol: string;
+  x: number;
+  y: number;
 };
 
-function part1(_input: string[]) {
-  const width = _input[0].length;
-  const height = _input.length;
+const isNumber = (n: string) => !isNaN(parseInt(n));
 
-  let sum = 0;
+const findNumbers = (input: string[]): numberData[] => {
+  const numbers: numberData[] = [];
 
-  for (let i = 0; i < height; i++) {
-    let currNumberStack = [];
-    let symbolNear = false;
+  for (let y = 0; y < input.length; y++) {
+    let numStack = [];
+    let x1 = 0;
+    let x2 = 0;
 
-    const row = _input[i];
+    for (let x = 0; x < input[y].length; x++) {
+      const currChar = input[y][x];
 
-    for (let j = 0; j < width; j++) {
-      const currChar = row[j];
-
-      if (!isNumber(currChar)) {
-        // if we tracked a symbol nearby and we have a numbers in the stacks
-        if (symbolNear && currNumberStack.length > 0) {
-          const currNum = parseInt(currNumberStack.join(''));
-          sum = sum + currNum;
+      if (isNumber(currChar)) {
+        if (!numStack.length) {
+          x1 = x;
+        } else {
+          x2 = x;
         }
-        symbolNear = false;
-        currNumberStack = [];
+        numStack.push(currChar);
       } else {
-        currNumberStack.push(currChar);
+        if (numStack.length) {
+          const value = parseInt(numStack.join(''));
+          numbers.push({ value, x1, y, x2 });
+          numStack = [];
+        }
       }
-
-      if (!symbolNear && currNumberStack.length > 0)
-        symbolNear = checkIfSymbolNearby(_input, i, j, width, height);
     }
   }
+
+  return numbers;
+};
+
+const isSymbol = (n: string) => n !== '.' && !isNumber(n);
+
+const findSymbols = (input: string[]): symbolData[] => {
+  const symbols: symbolData[] = [];
+
+  for (let y = 0; y < input.length; y++) {
+    for (let x = 0; x < input[y].length; x++) {
+      const currChar = input[y][x];
+      if (isSymbol(currChar)) {
+        symbols.push({ symbol: currChar, x, y });
+      }
+    }
+  }
+
+  return symbols;
+};
+
+const isPartNumber = (
+  n: numberData,
+  symbols: symbolData[],
+  xMax: number,
+  yMax: number,
+) => {
+  // form an array of check coordinates that are around the number all the length of the number
+  // ignore any coordinates that are outside the grid
+
+  const checkCoords = [];
+
+  // check the top and bottom of the number
+  for (let x = n.x1 - 1; x <= n.x2 + 1; x++) {
+    checkCoords.push({ x, y: n.y - 1 });
+    checkCoords.push({ x, y: n.y + 1 });
+  }
+
+  // check the left and right of the number
+  for (let y = n.y - 1; y <= n.y + 1; y++) {
+    checkCoords.push({ x: n.x1 - 1, y });
+    checkCoords.push({ x: n.x2 + 1, y });
+  }
+
+  // check the corners of the number
+  checkCoords.push({ x: n.x1 - 1, y: n.y - 1 });
+  checkCoords.push({ x: n.x2 + 1, y: n.y - 1 });
+  checkCoords.push({ x: n.x1 - 1, y: n.y + 1 });
+  checkCoords.push({ x: n.x2 + 1, y: n.y + 1 });
+
+  const checkCoordsFiltered = checkCoords.filter(
+    (c) => c.x >= 0 && c.x < xMax && c.y >= 0 && c.y < yMax,
+  );
+
+  const isSymbolAdjacent = checkCoordsFiltered.some((c) =>
+    symbols.some((s) => s.x === c.x && s.y === c.y),
+  );
+
+  return isSymbolAdjacent;
+};
+
+const getPartNumbers = (
+  numbers: numberData[],
+  symbols: symbolData[],
+  xMax: number,
+  yMax: number,
+) => {
+  const partNumbers = numbers.filter((n) =>
+    isPartNumber(n, symbols, xMax, yMax),
+  );
+
+  return partNumbers;
+};
+
+function part1(_input: string[]): number {
+  const xMax = _input[0].length;
+  const yMax = _input.length;
+
+  const numbers = findNumbers(_input);
+  const symbols = findSymbols(_input);
+  const partNumbers = getPartNumbers(numbers, symbols, xMax, yMax);
+  const partNumbersValues = partNumbers.map((n) => n.value);
+
+  const sum = partNumbersValues.reduce((a, b) => a + b, 0);
 
   return sum;
 }
